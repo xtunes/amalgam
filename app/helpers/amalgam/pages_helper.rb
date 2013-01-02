db8a43aecd7ed88fe79bdf939441fdd0746843f5
johnable <johnable@gmail.com>
2013-01-02 10:59:24 +0800

    修正了页面链接helper

--- app/helpers/amalgam/pages_helper.rb
+++ app/helpers/amalgam/pages_helper.rb
@@ -2,17 +2,13 @@ module Amalgam
   module PagesHelper
     def link_to_page(page,options={},&block)
       title = options.delete(:title) || page.title
-      url   = options.delete(:url) || page_path(page.path)
+      url   = options.delete(:url) || page_path(page.slug)
       condition = options.delete(:if)
       if condition.nil?
         link_to(title,url,options,&block)
       else
         link_to_if(condition,title,url,options,&block)
       end
-    end
-
-    def path_by_slug(page)
-      page_path(page.path)
     end
   end
 end
