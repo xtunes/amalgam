f1210ff01204322a55f10afdf5bdcadc706ce254
johnable <johnable@gmail.com>
2013-04-16 21:17:26 +0800

    增加了tree的图标显示

--- spec/dummy/app/models/page.rb
+++ spec/dummy/app/models/page.rb
@@ -4,15 +4,22 @@ class Page < ActiveRecord::Base
   include Amalgam::Types::Taggable
   cattr_accessor :admin_attrs
   attr_accessible :created_at, :as => :admin
-  self.admin_attrs = [:slug, :title, :identity,:created_at]
+  self.admin_attrs = [:slug, :title, :identity,:created_at, :redirect]
   has_many :posts
 
   taggable
-  taggable_as :skills
 
   def template_keys
     keys = []
     keys << self.slug
     keys
+  end
+
+  alias_method :old_to_nodes, :to_nodes
+
+  def to_nodes(fields=[])
+    hash = old_to_nodes(fields)
+    hash['attr']['class'] = "redirect" if self.redirect
+    hash
   end
 end
