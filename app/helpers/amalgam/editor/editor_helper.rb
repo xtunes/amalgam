792c8c2ac4b71f6697445a1c33a795955de08a60
johnable <johnable@gmail.com>
2013-04-22 18:22:50 +0800

    修改了图片编辑链接

--- app/helpers/amalgam/editor/editor_helper.rb
+++ app/helpers/amalgam/editor/editor_helper.rb
@@ -16,31 +16,55 @@ module Amalgam
     end
 
     def editable_image_tag obj,field,options={}
+      options[:display] ||= '300x300'
       if can_edit?
         options[:data] = (options[:data] || {}).merge(:type => 'editable')
         options[:data][:mercury] = "image"
         options[:data][:id] = "#{obj.class.to_s.tableize}/#{obj.id.to_s}.#{field.to_s}"
-        options[:data][:thumb] = options[:thumb]
+        options[:data][:thumb] = options[:display]
       end
-      options[:default_bg_color] ||= '456'
-      options[:default_front_color] ||= 'fff'
-      options[:default] ||= :dummyimage
       value = fetch_field(obj,field)
-      image_url = case options[:default]
-      when String
-        then options[:default].match(/^(http:\/\/)|^(https:\/\/)/) ? options[:default] : image_path(options[:default])
-      when Symbol
-        then "http://dummyimage.com/#{options[:thumb].match(/\d*(X|x)\d*/).to_s}/#{options[:default_bg_color]}/#{options[:default_front_color]}.gif"
+      options[:placeholder] ||= :dummyimage
+      options[:placeholder_options] ||= {}
+      if options[:placeholder].is_a?(String) && !value
+        if options[:display]
+          options[:width] ||= options[:display].match(/(\d*)(x|X)$/)[1] if options[:display].match /(\d*)(x|X)$/
+          options[:height] ||= options[:display].match(/^(x|X)(\d*)/)[2] if options[:display].match /^(x|X)(\d*)/
+          if options[:display].match /(\d*)(X|x)(\d*)/
+            options[:width] ||= options[:display].match(/(\d*)(X|x)(\d*)/)[1]
+            options[:height] ||= options[:display].match(/(\d*)(X|x)(\d*)/)[3]
+          end
+        end
+        options[:width] ||= options[:placeholder_options][:width]
+        options[:height] ||= options[:placeholder_options][:height]
       end
-      options.delete(:default)
+      options[:placeholder_options][:bg_color] ||= 456
+      options[:placeholder_options][:front_color] ||= 'fff'
+      options[:placeholder_options][:width] ||= (options[:width] || 300)
+      options[:placeholder_options][:height] ||= (options[:height] || 400)
+      options[:placeholder_options][:text] ||= "#{options[:placeholder_options][:width]}X#{options[:placeholder_options][:height]}"
+      image_url = case options[:placeholder]
+      when String
+        then options[:placeholder].match(/^(http:\/\/)|^(https:\/\/)/) ? options[:placeholder] : image_path(options[:placeholder])
+      when Symbol
+        then
+          if options[:display] && options[:display].match(/\d*(X|x)\d*/)
+            "http://dummyimage.com/#{options[:display].match(/\d*(X|x)\d*/).to_s}/#{options[:bg_color]}/#{options[:front_color]}.gif&text=#{options[:placeholder_options][:text]}"
+          else
+            "http://dummyimage.com/#{options[:placeholder_options][:width]}x#{options[:placeholder_options][:height]}/#{options[:bg_color]}/#{options[:front_color]}.gif&text=#{options[:placeholder_options][:text]}"
+          end
+      end
+      options.delete(:placeholder)
+      options.delete(:placeholder_options)
       if value
-        if options[:thumb]
-          image_url = Amalgam::Models::Image.find(value.to_i).file.thumb(options[:thumb]).url
+        if options[:display]
+          image_url = Amalgam::Models::Image.find(value.to_i).file.thumb(options[:display]).url
         else
           image_url = Amalgam::Models::Image.find(value.to_i).url
         end
         options[:data][:image_id] ||= value if can_edit?
       end
+      options.delete(:display)
       image_tag(image_url.to_s.html_safe,options)
     end
 
